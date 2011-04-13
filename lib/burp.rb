933d36c32e7b0ce4762fa5a19440d638e08eac6f
Dave M <dmarti21@gmail.com>
2011-04-13 16:27:02 +0000

    Supports arbitrary classes (not just hashes) and blocks as parameters

--- lib/burp.rb
+++ lib/burp.rb
@@ -1,31 +1,66 @@
 class Burp < Hash
   attr_reader :left_overs
-  #Converts an array of hashes into a hash
+  #Converts a list of data (i.e. an array) into a ruby hash structure
+  #
   #It takes as arguments,the array of hashes (each hash representing node data)
   #and a field in the hash (node data) to use as the key for the returned hash
-  def initialize(node_list, id)
+  def initialize(node_list, id_key, data_filter = nil)
     @left_overs = []
-    node_list.map do |node|
-      #better error information and eliminate nil keys
-      node = validate(node, id) 
-      #it's left over if the primary hash key doesn't exist in the node
-      add_leftovers(self, @left_overs, node) unless ( node[id] && node.keys.include?(id) )
-    end
-  end
-  
-  def validate(node, id)
-    begin
-      self[node[id]] = node 
-    rescue TypeError  #replacing an unhelpful error with something more helpful
-      raise ArgumentError, "Can't use #{id.inspect} as a valid key in #{node.inspect}. "\
-        "Maybe array elements are not hashes?" 
+    node_list.map do |raw_node|
+      node_id = find_id(raw_node, id_key)
+      if node_ok?(raw_node, node_id)
+        node_burp(raw_node, node_id, data_filter)  #adds to self (i.e. this hash)
+      else
+        @left_overs << raw_node
+      end
     end
   end
 
-  def add_leftovers(main_hash, left_overs, node)
-    #remove nil hash from main_hash
-    main_hash.delete(nil)
-    #add data to left overs (excluding nil data)
-    left_overs << node if node
+  def node_ok?(raw_node, node_id)
+    resp = true
+    resp = false if self.has_key?(node_id)
+    resp = false if raw_node && node_id.nil?
+    resp
+  end
+  
+  def node_burp(raw_node, node_id, data_filter)
+    node_data = find_data(raw_node, data_filter)
+    self[node_id] = node_data
+  end
+
+
+  def find_id(node, id_key)
+    if id_key.is_a? Proc
+      id_key.call(node)
+    else
+      validate(node, id_key)
+    end
+  end
+  
+  def find_data(node, data_filter)
+    return node unless data_filter
+    if data_filter.is_a? Proc
+      node_data = data_filter.call(node)
+    else
+      #Done this way to allow any object that supports [], []= and has_key? methods
+      #I'm intentionally avoiding Hash#select/reject for this (selfish) reason
+      data_filter = [data_filter].flatten
+      new_data = {}
+      data_filter.each do |key|
+        if x.has_key?(key)
+          new_data[key] = validate(node, key)
+        end
+      end
+      new_data
+    end
+  end
+
+  def validate(node, id)
+    begin
+      node[id]
+    rescue TypeError  #replacing an unhelpful error with something more helpful
+      raise ArgumentError, "Can't use #{id.inspect} as a valid key in #{node.inspect}. "\
+        "Maybe you're trying a hash method on something other than a hash?"
+    end
   end
 end
--- spec/burp_spec.rb
+++ spec/burp_spec.rb
@@ -1,11 +1,48 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
+module BurpSpecH
 
-describe "Burp" do
+  #Food Stuff for Advanced Burp
+  class Food
+    attr_accessor :name, :tastiness, :prep_effort
+    def initialize(name, tastiness, prep_effort)
+      @name = name
+      @tastiness = tastiness
+      @prep_effort = prep_effort
+    end
+  end
+
+  Foods = {'bacon' => {:taste =>  'awesome', :prep => 'nuke it'},
+           'tofu' => {:taste => 'meh', :prep => 'mix it in something'},
+           'beer' => {:taste => 'essential to life', :prep => 'mooch from buddy'}
+          }
+  FoodNames = Foods.keys
+
+  def self.make_food
+    #make us an array of Food objects
+    FoodNames.map do |food_name| Food.new(food_name, 
+                                        Foods[food_name][:taste],
+                                        Foods[food_name][:prep])
+    end
+  end
+
+  #Baseic stuff for Basic (and Advanced) Burp
+
+  NiceArray = [{:id => :a, :data => "A"},
+               {:id => :b, :data => "B"},
+               {:id => :c, :data => "C"}]
+ 
+  #notice the last id typo
+  DirtyArray = [{:id => :a, :data => "A"},
+                {:id => :b, :data => "B"},
+                {:id => :c, :data => "C"},
+                {:iidd => :d, :data => "D"}]
+end
+
+describe "Basic Burp" do
   before(:each) do
-    @nice_array = [{:id => :a, :data => "A"},
-                   {:id => :b, :data => "B"},
-                   {:id => :c, :data => "C"}]
+    @nice_array = BurpSpecH::NiceArray
+
     @nice_burped = {:a => {:id => :a, :data => "A"},
                     :b => {:id => :b, :data => "B"},
                     :c => {:id => :c, :data => "C"}}
@@ -13,10 +50,7 @@ describe "Burp" do
     @not_an_array = @nice_burped #its a hash now
 
     #notice the last id typo
-    @dirty_array = [{:id => :a, :data => "A"},
-                   {:id => :b, :data => "B"},
-                   {:id => :c, :data => "C"},
-                   {:iidd => :d, :data => "D"}]
+    @dirty_array = BurpSpecH::DirtyArray
 
     @dirty_burped = @nice_burped
 
@@ -37,4 +71,60 @@ describe "Burp" do
     my_dirty_burp.should == @dirty_burped
     my_dirty_burp.left_overs.should == @dirty_left_overs
   end
+end
+
+describe "Advanced Burp" do
+  include BurpSpecH
+
+  before(:each) do
+    @food_list = BurpSpecH.make_food
+    @id_block = lambda{|item| item.name}
+    @filtered_food_block = lambda{|item| item.prep_effort} 
+    @filtered_simple_block = lambda{|item| item[:data].downcase}                    
+    @nice_array = NiceArray
+    @nice_burped_proc = {:a => "a",
+                         :b => "b",
+                         :c => "c"}
+    @dirty_array = DirtyArray
+    @dirty_bupred_proc = @nice_burped_proc
+    @dirty_left_overs = [{:iidd => :d, :data => "D"}]
+    @filtered_burp = {'bacon' => 'nuke it',
+                      'tofu' => 'mix it in something',
+                      'beer' => 'mooch from buddy'}
+  end
+
+  it "is passed a food list" do
+    @food_list.should be_an Array
+  end
+
+  it "uses blocks for ids without vomiting" do
+    my_burp = Burp.new(@food_list, @id_block)
+    my_burp['bacon'].should be_a Food
+    my_burp['bacon'].tastiness.should == "awesome"
+    my_burp['bacon'].prep_effort.should == "nuke it"
+    
+    #comprehensive test
+    FoodNames.each do |food_name|
+      my_burp[food_name].should be_a Food
+      my_burp[food_name].tastiness.should == Foods[food_name][:taste]
+      my_burp[food_name].prep_effort.should == Foods[food_name][:prep]
+    end
+  end
+
+  it "uses blocks for nice data without vomiting" do
+    my_burp = Burp.new(@nice_array, :id, @filtered_simple_block)
+    my_burp.should == @nice_burped_proc
+  end
+
+  it "uses blocks for dirty data without vomiting" do
+    my_dirty_burp = Burp.new(@dirty_array, :id, @filtered_simple_block)
+    my_dirty_burp.should == @nice_burped_proc
+    my_dirty_burp.left_overs.should == @dirty_left_overs
+  end
+
+  it "uses blocks for both ids and data without vomiting" do
+    my_burp = Burp.new(@food_list, @id_block, @filtered_food_block)
+    my_burp.should == @filtered_burp
+  end
+
 end
